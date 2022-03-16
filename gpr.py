b519727600de4833b1c04c7dbb660b8850577019
Ben Lyons <bencl1998@gmail.com>
2022-03-16 15:24:29 -0400

    Improve gp regression

--- gpr.py
+++ gpr.py
@@ -4,46 +4,90 @@ import numpy as np
 import random
 
 import pickle
+import sys
+
 
 def main():
     # load the data
     random.seed(7)
     stats = []
     spreads = []
-    with open("spread_training_data.csv", "r") as f:
-        rows = []
-        for row in f.readlines():
-            row = [float(x) for x in row.strip().split(',')]
-            rows.append(row)
-        random.shuffle(rows)
-        for row in rows:
-            stats.append(np.array(row[:-1]))
-            spreads.append(np.array(row[-1]))
 
-    size = len(stats)
-    training_size = int(size*.8)
-    validation_size = size - training_size
+    if len(sys.argv) == 1:
+        print("Need arguments.")
+        sys.exit(0)
 
-    training_data = np.array(stats[:training_size])
-    training_outputs = np.array(spreads[:training_size])
-    validation_data = np.array(stats[training_size:])
-    validation_outputs = np.array(spreads[training_size:])
+    if sys.argv[1] == "--train":
+        with open("spread_training_data.csv", "r") as f:
+            rows = []
+            for row in f.readlines():
+                row = [float(x) for x in row.strip().split(',')]
+                rows.append(row)
+            random.shuffle(rows)
+            for row in rows:
+                stats.append(np.array(row[:-1]))
+                spreads.append(np.array(row[-1]))
 
-    kernel = 2*RBF(length_scale=1.0, length_scale_bounds=(1e-2, 100)) 
-    noise = 4
-    gp = GaussianProcessRegressor(kernel=kernel, alpha=noise**2,  n_restarts_optimizer=9)
-    gp.fit(training_data, training_outputs)
+        size = len(stats)
+        training_size = int(size*.8)
+        validation_size = size - training_size
 
-    mean_prediction, std_prediction = gp.predict(validation_data, return_std=True)
-    avg_error = np.sum(np.abs(np.subtract(mean_prediction, validation_outputs)))/len(validation_outputs)
+        training_data = np.array(stats[:training_size])
+        training_outputs = np.array(spreads[:training_size])
+        validation_data = np.array(stats[training_size:])
+        validation_outputs = np.array(spreads[training_size:])
 
-    print(mean_prediction)
-    print(validation_outputs)
-    print(std_prediction)
-    print(avg_error)
+        kernel = RBF(length_scale=1.0, length_scale_bounds=(1e-2, 100)) 
+        noise = 2
+        gp = GaussianProcessRegressor(kernel=kernel, alpha=noise**2,  n_restarts_optimizer=9)
+        gp.fit(training_data, training_outputs)
 
-    with open("gp_model.pkl", "wb") as f:
-        pickle.dump(gp, f)
+        mean_prediction, std_prediction = gp.predict(validation_data, return_std=True)
+        avg_error = np.sum(np.abs(np.subtract(mean_prediction, validation_outputs)))/len(validation_outputs)
+
+        print(mean_prediction)
+        print(validation_outputs)
+        print(std_prediction)
+        print(avg_error)
+
+        with open("gp_model.pkl", "wb") as f:
+            pickle.dump(gp, f)
+
+    elif sys.argv[1] == "--eval":
+        with open("gp_model.pkl", "rb") as f:
+            gp = pickle.load(f)
+
+        with open("training_data.csv", "r") as f:
+            rows = []
+            for row in f.readlines():
+                row = [float(x) for x in row.strip().split(',')]
+                rows.append(row)
+            random.shuffle(rows)
+            for row in rows:
+                stats.append(np.array(row[:-1]))
+                spreads.append(np.array(row[-1]))
+
+        size = len(stats)
+        training_size = int(size*.8)
+        validation_size = size - training_size
+
+        training_data = np.array(stats[:training_size])
+        training_outputs = np.array(spreads[:training_size])
+        validation_data = np.array(stats[training_size:])
+        validation_outputs = np.array(spreads[training_size:])
+
+        mean_prediction, std_prediction = gp.predict(validation_data, return_std=True)
+
+        spreads = mean_prediction.tolist()
+
+        correct = 0
+        for s, r in zip(spreads, validation_outputs):
+            if s > 0 and r > 0 or s < 0 and r == 0:
+                correct += 1
+        acc = correct / len(spreads)
+        
+        print(validation_outputs)
+        print(f"Classification Accuracy: {acc}")
         
 
 if __name__ == "__main__":
