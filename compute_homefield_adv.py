15e1e389b011e25aa6e89759bcef859cefbd0f36
Ben Lyons <bencl1998@gmail.com>
2022-03-22 12:01:16 -0400

    Infer noise mean for homefield adv

--- compute_homefield_adv.py
+++ compute_homefield_adv.py
@@ -6,21 +6,21 @@ import numpy as np
 # => P(Adv | Obs) oc P(Obs | Adv)*P(Adv)
 # Assume P(Adv) = N(prior_mean, prior_var)
 # we claim that each homefield adv is gaussian
-def posterior(prior_mean, prior_var, sigma_n2, obs):
-    posterior_mean = (obs*prior_var + prior_mean*sigma_n2)/ (prior_var + sigma_n2)
+def posterior(prior_mean, prior_var, mu_n, sigma_n2, obs):
+    posterior_mean = (prior_var*(obs - mu_n) + prior_mean*sigma_n2)/ (prior_var + sigma_n2)
     posterior_var = (prior_var*sigma_n2)/(prior_var + sigma_n2)
     return posterior_mean, posterior_var
 
 # derived via maximum likelihood estimation
-# returns the variance due to noise
+# returns mean and variance of noise
 def estimate_noise(prior_mean, prior_var, data):
-    first_term = np.sum(np.square(np.subtract(data, prior_mean))) / len(data)
-    sigma_n2 = first_term - prior_var
-    return sigma_n2
+    mu_n = np.sum(np.subtract(data, prior_mean)) / len(data)
+    sigma_n = np.sum(np.square(np.subtract(np.subtract(data, prior_mean), mu_n))) / len(data) - prior_var
+    return mu_n, sigma_n
 
 def compute_homefield_advs():
     prior_mean = 3.5
-    prior_var = 1.5
+    prior_var = 1.0
     games_df = pd.read_csv("data/all_game_logs.csv")
     teams_df = pd.read_csv("data/all_team_stats.csv")
 
@@ -49,15 +49,20 @@ def compute_homefield_advs():
         homefield_diffs[team] = adv
 
     data = np.array(list(homefield_diffs.values()))
-    sigma_n2 = estimate_noise(prior_mean, prior_var, data)
+    mu_n, sigma_n2 = estimate_noise(prior_mean, prior_var, data)
+
+    print("Noise mean:", mu_n)
+    print("Noise std:", sigma_n2)
 
     homefield_adv = {}
     for team, val in homefield_diffs.items():
-        adv, std = posterior(prior_mean, prior_var, sigma_n2, val)
+        adv, std = posterior(prior_mean, prior_var, mu_n, sigma_n2, val)
         homefield_adv[team] = adv
     advs = np.array(list(homefield_adv.values()))
     new_mean = np.mean(advs)
     new_var = np.var(advs)
+    print(new_mean)
+    print(new_var)
 
     return homefield_adv
 
